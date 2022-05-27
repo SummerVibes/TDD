86d03374e1066e94a7945d7e37cb1dc379237b4f
xuebling <asxxw741@qq.com>
2022-05-27 22:13:46 +0800

    new URL + view for adding to existing lists. FT passed :-)

--- functional_tests/test.py
+++ functional_tests/test.py
@@ -71,6 +71,7 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
+
         self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
         inputbox = self.browser.find_element_by_id('id_new_item')
